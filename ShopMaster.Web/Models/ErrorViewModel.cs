026452a413f850917752aebe7f91ae449abf520e
yasincmc55 <53167111+yasincmc55@users.noreply.github.com>
2025-07-15 11:44:54 +0300

    Add project files.

---
+++ ShopMaster.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMaster.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
