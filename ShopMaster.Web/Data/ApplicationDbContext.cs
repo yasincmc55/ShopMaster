7c96b2809e3cc55993507412522cbb78731bc46e
yasincmc55 <53167111+yasincmc55@users.noreply.github.com>
2025-07-18 10:22:22 +0300

    Database Connections Settings

---
+++ ShopMaster.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopMaster.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
