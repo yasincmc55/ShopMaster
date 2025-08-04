a14214c8c00afb8c4af33978fbc00792a89c96b5
yasincmc55 <53167111+yasincmc55@users.noreply.github.com>
2025-08-04 18:02:34 +0300

    Edit and update

--- ShopMaster.Web/Controllers/CategoryController.cs
+++ ShopMaster.Web/Controllers/CategoryController.cs
@@ -30,11 +30,55 @@ namespace ShopMaster.Web.Controllers
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match The Name");
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Category Name");
             }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        /*
+         * GET için: int? id al, DB’den kendin çek.
+         * POST için: Category category alabilirsin, çünkü formdan gelen verileri model binder kendisi doldurur.
+         */
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+               return NotFound();
+            
+            Category? category = _db.Categories.Find(id);
+            //Category? category_1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? category_2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if(category == null)
+               return NotFound();
+            
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Category Name");
+            }
+
+            bool isDuplicateDisplayOrder = _db.Categories.Any(c => c.DisplayOrder == obj.DisplayOrder && c.Id != obj.Id);
+            if (isDuplicateDisplayOrder)
+            {
+                ModelState.AddModelError("DisplayOrder", "This order is already assigned to another category");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
